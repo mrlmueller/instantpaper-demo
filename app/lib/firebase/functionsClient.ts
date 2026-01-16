8adfc537dff5d1c6f355c0d4a597d643f94109db
mrlmueller <132136867+mrlmueller@users.noreply.github.com>
2026-01-16 19:36:39 +0100

    change location of stripe endpoint

--- app/lib/firebase/functionsClient.ts
+++ app/lib/firebase/functionsClient.ts
@@ -3,7 +3,6 @@ import { getFunctions } from "firebase/functions";
 import { firebaseApp } from "./config";
 
 const FUNCTIONS_REGION =
-  process.env.NEXT_PUBLIC_FIREBASE_FUNCTIONS_REGION || "us-central1";
+  process.env.NEXT_PUBLIC_FIREBASE_FUNCTIONS_REGION || "europe-west3";
 
 export const functionsClient = getFunctions(firebaseApp, FUNCTIONS_REGION);
-
